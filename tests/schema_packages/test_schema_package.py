9546cc450618966cca38d9c22a42d7647f978655
Martin Dobiasch <martin.dobiasch@donau-uni.ac.at>
2025-02-10 09:54:04 +0100

    Fix tests for schema_package

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -4,8 +4,9 @@ from nomad.client import normalize_all, parse
 
 
 def test_schema_package():
-    test_file = os.path.join('tests', 'data', 'test.archive.yaml')
+    test_file = os.path.join('tests', 'data', 'test_cube.archive.yaml')
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
 
-    assert entry_archive.data.message == 'Hello Markus!'
+    # TODO: we should have some meaningful tests here ...
+    assert len(entry_archive.data.steps) == 80 # noqa: PLR2004
