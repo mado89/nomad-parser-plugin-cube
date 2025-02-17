a97566141cc585034baa23137f23e52bf9464884
Martin Dobiasch <martin.dobiasch@donau-uni.ac.at>
2025-02-17 16:19:01 +0100

    fix? regexp warning

--- src/cube/parsers/__init__.py
+++ src/cube/parsers/__init__.py
@@ -15,5 +15,5 @@ class NewParserEntryPoint(ParserEntryPoint):
 parser_entry_point = NewParserEntryPoint(
     name='NewParser',
     description='New parser entry point configuration.',
-    mainfile_name_re='.*\.dat',
+    mainfile_name_re='.*dat',
 )
