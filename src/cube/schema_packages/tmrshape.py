1b7b17adb63234f5d368d1d56303ef336a5c74a5
Martin Dobiasch <martin.dobiasch@donau-uni.ac.at>
2025-02-24 16:43:25 +0100

    ccc

--- src/cube/schema_packages/tmrshape.py
+++ src/cube/schema_packages/tmrshape.py
@@ -112,34 +112,34 @@ class EllipseConfig(SimulationConfigShape):
       description='Radius 1',
       a_eln={
           "component": "NumberEditQuantity",
-          "defaultDisplayUnit": "nanometres"
+          "defaultDisplayUnit": "m^-9"
       },
-      unit="nanometres",
+      unit="m^-9",
   )
   init_r2 = Quantity(
       type=np.float64,
       description='Radius 2',
       a_eln={
           "component": "NumberEditQuantity",
-          "defaultDisplayUnit": "nanometres"
+          "defaultDisplayUnit": "m^-9"
       },
-      unit="nanometres",
+      unit="m^-9",
   )
   init_h = Quantity(
       type=np.float64,
       description='Height',
       a_eln={
           "component": "NumberEditQuantity",
-          "defaultDisplayUnit": "nanometres"
+          "defaultDisplayUnit": "m^-9"
       },
-      unit="nanometres",
+      unit="m^-9",
   )
 
   def setFromDict(self, _dict:dict) -> None:
     super().setFromDict(_dict)
-    self.init_r1 = ureg.Quantity(float(_dict['init_r1']), 'nanometres')
-    self.init_r2 = ureg.Quantity(float(_dict['init_r2']), 'nanometres')
-    self.init_h = ureg.Quantity(float(_dict['init_h']), 'nanometres')
+    self.init_r1 = ureg.Quantity(float(_dict['init_r1']), 'm^-9')
+    self.init_r2 = ureg.Quantity(float(_dict['init_r2']), 'm^-9')
+    self.init_h = ureg.Quantity(float(_dict['init_h']), 'm^-9')
 
 class BoxConfig(SimulationConfigShape):
   m_def= Section(
@@ -156,34 +156,34 @@ class BoxConfig(SimulationConfigShape):
       description='Initial length x',
       a_eln={
           "component": "NumberEditQuantity",
-          "defaultDisplayUnit": "nanometres"
+          "defaultDisplayUnit": "m^-9"
       },
-      unit="nanometres",
+      unit="m^-9",
   )
   init_ylen = Quantity(
       type=np.float64,
       description='Initial length y',
       a_eln={
           "component": "NumberEditQuantity",
-          "defaultDisplayUnit": "nanometres"
+          "defaultDisplayUnit": "m^-9"
       },
-      unit="nanometres",
+      unit="m^-9",
   )
   init_zlen = Quantity(
       type=np.float64,
       description='Initial length z',
       a_eln={
           "component": "NumberEditQuantity",
-          "defaultDisplayUnit": "nanometres"
+          "defaultDisplayUnit": "m^-9"
       },
-      unit="nanometres",
+      unit="m^-9",
   )
 
   def setFromDict(self, _dict:dict) -> None:
     super().setFromDict(_dict)
-    self.init_xlen = ureg.Quantity(float(_dict['init_xlen']), 'nanometres')
-    self.init_ylen = ureg.Quantity(float(_dict['init_ylen']), 'nanometres')
-    self.init_zlen = ureg.Quantity(float(_dict['init_zlen']), 'nanometres')
+    self.init_xlen = ureg.Quantity(float(_dict['init_xlen']), 'm^-9')
+    self.init_ylen = ureg.Quantity(float(_dict['init_ylen']), 'm^-9')
+    self.init_zlen = ureg.Quantity(float(_dict['init_zlen']), 'm^-9')
 
 class SimulationConfigSimulation(ArchiveSection):
   m_def = Section(
